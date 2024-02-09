471bcb8abd4c79381f4b17558bead5e7a62945df
scotty974 <fabienethevepro@gmail.com>
2024-02-09 21:42:32 +0100

    style header

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
-<section>
-  <h1>Hey</h1>
-</section>
+<script>
+  import Header from "./components/header/Header.svelte";
+</script>
 
+<section class="container m-auto">
+  <header class="flex justify-center my-4"><Header></Header></header>
+</section>
---
+++ src/components/header/Header.svelte
@@ -0,0 +1,6 @@
+<ul class="flex w-96 justify-between items-center">
+    <li class="flex items-center" >Work <span class="flex items-center justify-center ml-2 pb-1 border w-6 h-6 text-sm rounded">/</span></li>
+    <li>A Propos</li>
+    <li>Projets</li>
+    <li>Contacts</li>
+</ul>
